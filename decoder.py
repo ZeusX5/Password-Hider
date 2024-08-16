a9421a8e884d9e6d636e07b493c0a8b64af41858
Victor <garciavic5@gmail.com>
2024-08-16 09:18:47 -0500

    Update decoder.py
    Cleaned up password prompt

--- decoder.py
+++ decoder.py
@@ -1,23 +1,13 @@
 # prompt for file name
 encode_file = input('Enter file: ')
 
-# prompt for 12 indexes
-passcode_1 = int(input('Passcode 1: '))
-passcode_2 = int(input('Passcode 2: '))
-passcode_3 = int(input('Passcode 3: '))
-passcode_4 = int(input('Passcode 4: '))
-passcode_5 = int(input('Passcode 5: '))
-passcode_6 = int(input('Passcode 6: '))
-passcode_7 = int(input('Passcode 7: '))
-passcode_8 = int(input('Passcode 8: '))
-passcode_9 = int(input('Passcode 9: '))
-passcode_10 = int(input('Passcode 10: '))
-passcode_11 = int(input('Passcode 11: '))
-passcode_12 = int(input('Passcode 12: '))
-
-# retrieve 12-word using index list
-code = [passcode_1, passcode_2, passcode_3, passcode_4, passcode_5, passcode_6,
-        passcode_7, passcode_8, passcode_9, passcode_10, passcode_11, passcode_12]
+# prompt for 12 index password
+password = []
+count = 1
+while count <= 12:
+    code = int(input(f'Word {count}: '))
+    password.append(code)
+    count += 1
 
 # open file
 words = []
@@ -27,5 +17,5 @@ with open(f'{encode_file}.txt', 'r') as file:
         words.append(word)
 
 # return passcode list
-for dex in code:
+for dex in password:
     print(words[dex])
