33d1022627763fa6a5c7919192b2c8d6448aebc1
Victor <garciavic5@gmail.com>
2024-08-20 16:58:55 -0500

    Update decoder.py
    Added While loop to keep window open (when .exe) after retrieving password and 'exit' option to close script.

--- decoder.py
+++ decoder.py
@@ -1,21 +1,26 @@
-# prompt for file name
-encode_file = input('Enter file: ')
+from sys import exit
 
-# prompt for 12 index password
-password = []
-count = 1
-while count <= 12:
-    code = int(input(f'Code {count}: '))
-    password.append(code)
-    count += 1
+while True:
+    # prompt for file name
+    encode_file = input('Enter file or exit: ')
+    if encode_file.lower() == 'exit':
+        exit()
 
-# open file
-words = []
-with open(f'{encode_file}.txt', 'r') as file:
-    content = file.read().split()
-    for word in content:
-        words.append(word)
+    # prompt for 12 index password
+    password = []
+    count = 1
+    while count <= 12:
+        code = int(input(f'Code {count}: '))
+        password.append(code)
+        count += 1
 
-# return passcode list
-for dex in password:
-    print(words[dex])
+    # open file
+    words = []
+    with open(f'{encode_file}.txt', 'r') as file:
+        content = file.read().split()
+        for word in content:
+            words.append(word)
+
+    # return passcode list
+    for dex in password:
+        print(words[dex])
