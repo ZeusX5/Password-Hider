9e05c659dc1062013086224ac4c1e336da5ed5b5
Victor <garciavic5@gmail.com>
2024-08-30 12:38:07 -0500

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -2,17 +2,33 @@ from sys import exit
 
 while True:
     # prompt for file name
-    encode_file = input('Enter file or exit: ')
-    if encode_file.lower() == 'exit':
+    encode_file = input('Enter filename or exit: ')
+    if encode_file.lower() == 'exit' or len(encode_file) == 0:
+        exit()
+    
+    pass_len = input('Enter password length: ')
+    # trying to convert input to a number - if not, asking again.
+    while type(pass_len) != int: 
+        try:
+            pass_len = int(pass_len)
+        except:
+            print('Please enter a number.')
+            pass_len = input('Number of words: ')
+            
+    if pass_len == 0:
         exit()
 
-    # prompt for 12 index password
     password = []
     count = 1
-    while count <= 12:
-        code = int(input(f'Code {count}: '))
-        password.append(code)
-        count += 1
+    while count <= pass_len:
+        try:
+            code = int(input(f'Entry {count}: '))
+            password.append(code)
+            count += 1
+        except ValueError:
+            # exits because don't want to give a hint to what type of code it should be.
+            print('Incorrect code.')
+            exit()
 
     # open file
     words = []
